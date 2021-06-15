53f6460481303c54e93938c6a378a83ab5106526
XiaomeiLi1 <xiaomei.li@mymail.unisa.edu.au>
2021-06-15 13:48:36 +0930

    Symbols updated in the following order: 1. Ids in HGNC are kept. 2. for those not updated, check if they are in previous.Symbol. update. 3. if there is any not updated yet, check in alias and update.

--- R/updateGeneSymbol.r
+++ R/updateGeneSymbol.r
@@ -2,33 +2,90 @@ updateGeneSymbol <- function(IDs){
   if (!requireNamespace("tidyverse"))
     install.packages('tidyverse')
   library(tidyverse)
-  HGNC <- AMCBGeneUtils::HGNC
+  HGNC.Approved <- AMCBGeneUtils::HGNC%>%
+    filter(Status=="Approved")
 
   IDs <- str_trim(IDs)
   res <- matrix(nrow = length(IDs), ncol = 2)
   colnames(res) <- c("IDs","HGNC.symbol")
-  res[,1] <- IDs
-  # tidying up string data
-  aux <- HGNC%>%transmute(Approved.symbol
-                      , allNames = str_c(Approved.symbol,Previous.symbols
-                                           ,Alias.symbols,sep = ","))%>%
-    mutate(allNames=str_replace_all(allNames," ",""))%>%
-    mutate(allNames=str_replace_all(allNames,",+"," "))%>%
-    mutate(allNames=str_trim(allNames))
 
+  # Tidying up string data for previous symbols
+  previous <- HGNC.Approved%>%
+    transmute(Approved.symbol
+                      , Previous.symbols = str_c(Previous.symbols
+                                           ,sep = ","))%>%
+    mutate(Previous.symbols=str_replace_all(Previous.symbols," ",""))%>%
+    mutate(Previous.symbols=str_replace_all(Previous.symbols,",+"," "))%>%
+    mutate(Previous.symbols=str_trim(Previous.symbols))%>%
+    filter(Previous.symbols!="")
 
-  b <- apply(aux, MARGIN = 1
+  aux <- apply(previous, MARGIN = 1
              , function(x){
                temp <- t(str_split(x[2], " ", simplify = T))
-               temp <- cbind(rep(temp[1,1],ncol(temp)),temp)
+               temp <- cbind(rep(x[1, drop=T],ncol(temp)),temp)
              })
-  b <- do.call(what = rbind,args = b)
-  colnames(b) <- c("HGNC.symbol","symbol")
 
-  keepIndex <- IDs%in%b[,"HGNC.symbol"]
-  res[keepIndex,2] <- res[keepIndex,1]
-  res[!keepIndex,2] <- b[match(res[!keepIndex,1]
-                               ,as.matrix(b[,2])),1]
+  previous <- do.call(what = rbind,args = aux)
+  colnames(previous) <- c("HGNC.symbol","IDs")
+
+  # Tidying up string data for Alias
+  Alias <- HGNC.Approved%>%
+    transmute(Approved.symbol
+              , Alias.symbols = str_c(Alias.symbols
+                                         ,sep = ","))%>%
+    mutate(Alias.symbols=str_replace_all(Alias.symbols," ",""))%>%
+    mutate(Alias.symbols=str_replace_all(Alias.symbols,",+"," "))%>%
+    mutate(Alias.symbols=str_trim(Alias.symbols))%>%
+    filter(Alias.symbols!="")
+
+  aux <- apply(Alias, MARGIN = 1
+                , function(x){
+                  temp <- t(str_split(x[2], " ", simplify = T))
+                  temp <- cbind(rep(x[1, drop=T],ncol(temp)),temp)
+                })
+
+  Alias <- do.call(what = rbind,args = aux)
+  colnames(Alias) <- c("HGNC.symbol","IDs")
+
+
+  # Find those IDs already in HGNC$Approved.symbol
+  res[,1] <- IDs
+  index <- res[,1]%in%HGNC.Approved$Approved.symbol
+  res[index,2] <- res[index,1]
+
+  # Check if IDs are in previous
+  temp <- left_join(as_tibble(res[!index,1, drop=F])
+                    ,as_tibble(previous), by = "IDs")
+
+  if(sum(duplicated(temp$IDs))>0){
+    message("There are duplicated gene symbols. Keeping the first ocurrence.")
+    message("duplicated genes:")
+    print(temp$IDs[duplicated(temp$IDs)])
+    temp <- temp[!duplicated(temp$IDs),]
+  }
+  res[!index,2] <- temp$HGNC.symbol
+
+  # Check if IDs are in Alias
+  index <- is.na(res[,2])
+  if(sum(index)>0){
+    temp <- left_join(as_tibble(res[index,1, drop=F])
+                      ,as_tibble(Alias), by = "IDs")
+
+    if(sum(duplicated(temp$IDs))>0){
+      message("There are duplicated gene symbols. Keeping the first ocurrence.")
+      message("duplicated genes:")
+      print(temp$IDs[duplicated(temp$IDs)])
+      temp <- temp[!duplicated(temp$IDs),]
+    }
+    res[index,2] <- temp$HGNC.symbol
+  }
+
+
+
+  aux <- res[!index,2]
+  aux %in% previous[,2]
+
+
 
   #index <- match(res[,1],as.matrix(b[,2]))
   #res[,2] <- b[index,1]
